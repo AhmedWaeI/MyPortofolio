b9e8f70ef849c96fdf928aa98fe7ca7c035896ab
xSlayer <ahmed_the_best17@yahoo.com>
2024-03-05 20:46:25 +0200

    adding skills section

--- MyPortofolio.js
+++ MyPortofolio.js
@@ -1,5 +1,7 @@
 const intern= document.querySelector(".Intern");
 const image= document.querySelector(".Experience .Intern img");
+const skills= document.querySelector(".Skills .containerskills");
+const header= document.querySelector(".skills h2");
 
 intern.addEventListener("mouseover", function(){
     console.log("Mouse Over");
@@ -9,3 +11,9 @@ intern.addEventListener("mouseout", function(){
     console.log("Mouse Out");
     image.src="Images/noun-machine-learning-2010152.svg"
 });
+skills.addEventListener("mouseover", function(){
+    header.style.color= "#58E8C8";
+})
+skills.addEventListener("mouseout", function(){
+    header.style.color= "white";
+})
